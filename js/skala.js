2e1fda1424d4a4932d7f19bc6065ebc480a233d1
skakala <skala@localhost.localdomain>
2018-08-03 16:06:59 +0200

    paralax bcg 1

---
+++ js/skala.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+     
+  $('.navbar-item').each(function(e) {
+    $(this).click(function(){
+      if($('#navbar-burger-id').hasClass('is-active')){
+        $('#navbar-burger-id').removeClass('is-active');
+        $('#navbar-menu-id').removeClass('is-active');
+      }
+    });
+  });
+
+  // --------------------------
+  $('#navbar-burger-id').click(function () {
+    if($('#navbar-burger-id').hasClass('is-active')){
+      $('#navbar-burger-id').removeClass('is-active');
+      $('#navbar-menu-id').removeClass('is-active');
+    }else {
+      $('#navbar-burger-id').addClass('is-active');
+      $('#navbar-menu-id').addClass('is-active');
+    }
+  });
+// --------------------------
+
+  $('a[href^="#"]').click(function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    $('html, body').animate({
+    scrollTop: $(target).offset().top
+    }, 500);
+    return false;
+})
+
+
+// lottie.loadAnimation({
+//   container: testowe, // the dom element that will contain the animation
+//   renderer: 'svg',
+//   loop: true,
+//   autoplay: true,
+//   path: 'lottie/stopwatch.json' // the path to the animation json
+// });
+
+//ScrollMagic
+
+var controller = new ScrollMagic.Controller();
+
+//pin
+var pinIntroScene = new ScrollMagic.Scene({
+  triggerElement: '#svg-logo',
+  // offset: '180',
+  duration:'28%',
+  triggerHook: 0.7
+})
+//  .addIndicators({
+//    collorTrigger: 'black',
+//    startTrigger:'green'
+//  })
+.setPin('#intro', {pushFollowers: false})
+ .addTo(controller)
+// --------------
+ var pinIntroScene2 = new ScrollMagic.Scene({
+  triggerElement: '#about',
+  duration: '35%',
+  triggerHook: 0.4
+})
+// .addIndicators({
+//   collorTrigger: 'black',
+//   startTrigger:'green'
+// })
+.setPin('#intro', {pushFollowers: false})
+ .addTo(controller)
+
+
+var paralaxHeros = new ScrollMagic.Scene({
+triggerElement: '#paralaxHero',
+triggerHook:1,
+duration: '150%'
+})
+.setTween('#paralaxHero',{
+           backgroundPosition: "50% 10%",
+           ease: Linear.easeNone
+})
+// .setPin('#paralaxHero', {pushFollowers: false})
+.addIndicators({
+     collorTrigger: 'black',
+     startTrigger:'green'
+   })
+.addTo(controller);
+
+
+
+// --------------------------
+// --------------------------
+      }   
+    );
+
